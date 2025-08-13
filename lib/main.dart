6f76f060374affe3b02d79835cfa5743a7b9dfcd
Nufais <dev.cknufiii@gmail.com>
2025-08-13 15:13:54 +0530

    checking

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:crm_admin/core/widgets/admin_dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child:AdminDashboard() ,
         ),
       ),
     );
