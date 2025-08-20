4a761fdd9702dccece4e1d8d300dac1169adf734
Nufais <dev.cknufiii@gmail.com>
2025-08-20 12:11:45 +0530

    intial2

--- lib/core/constants/images.dart
+++ lib/core/constants/images.dart
@@ -3,6 +3,14 @@ const usericon = 'assets/icons/user.svg';
 const passicon = 'assets/icons/password.svg';
 const showicon = 'assets/icons/eye.svg';
 
+class SidebarIcons {
+  static const String home = "assets/icons/home.svg";
+  static const String dashboard = "assets/icons/register.svg";
+  static const String ticket = "assets/icons/ticket.svg";
+  static const String register = "assets/icons/register.svg";
+  static const String msg = "assets/icons/chat.svg";
+
+}
 //home_view icons
 const userTotal = 'assets/icons/user_total.svg';
 const employeeTotal = 'assets/icons/employee.svg';
@@ -15,3 +23,5 @@ const employeeReg = 'assets/icons/employee-reg.svg';
 const ticketReg = 'assets/icons/ticket-reg.svg';
 const softwareReg = 'assets/icons/software-reg.svg';
 const ticketAssign = 'assets/icons/ticket-assign.svg';
+
+
